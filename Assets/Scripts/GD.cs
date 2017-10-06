303ec05ffab9491014bd74ea9adfb763589dd765
henianming <1500923374@qq.com>
2017-10-06 16:23:56 +0800

    移动，中心缩放工作区域功能实现

---
+++ Assets/Scripts/GD.cs
@@ -0,0 +1,5 @@
+﻿public static class MB {
+	public static int LEFT = 0;
+	public static int RIGHT = 1;
+	public static int MIDDLE = 2;
+}
--- Assets/Scripts/NodeViews/HContentCtrl.cs
+++ Assets/Scripts/NodeViews/HContentCtrl.cs
@@ -2,27 +2,39 @@
 
 public class HContentCtrl : MonoBehaviour {
 	private static float sm_scaleSpeed = 0.05f;
+	private static float sm_scaleMin = 0.001f;
 
 	private Transform m_contentTs;
+	private RectTransform m_contentRTs;
 	private bool m_isMouseEnter;
 	private BoxCollider2D m_bc2d;
 	private RectTransform m_rts;
 	private float m_curScaleArg;
 
+	private Camera m_camera;
+	private bool m_isMiddleDrag;
+	private Vector3 m_midDragMousePos = new Vector3();
+	private Vector3 m_midDragNodePos = new Vector3();
+
 	#region Life
 
 	private void OnEnable() {
 		m_isMouseEnter = false;
+		m_isMiddleDrag = false;
 
 		m_curScaleArg = 1.0f;
 		m_contentTs = transform.Find("Content");
+		m_contentRTs = m_contentTs.gameObject.GetComponent<RectTransform>();
 		m_bc2d = gameObject.GetComponent<BoxCollider2D>();
 		m_rts = gameObject.GetComponent<RectTransform>();
+		m_camera = Camera.main;
 	}
 
 	private void OnDisable() {
+		m_camera = null;
 		m_rts = null;
 		m_bc2d = null;
+		m_contentRTs = null;
 		m_contentTs = null;
 	}
 
@@ -32,17 +44,42 @@ public class HContentCtrl : MonoBehaviour {
 		float h = r.yMax - r.yMin;
 		m_bc2d.size = new Vector2(w, h);
 
-		if (m_isMouseEnter == true && Input.mouseScrollDelta.y != 0.0f) {
-			float scaleChangeValue = Input.mouseScrollDelta.y * sm_scaleSpeed;
-			m_curScaleArg += scaleChangeValue;
-			if (m_curScaleArg < 0) {
-				m_curScaleArg = 0.0f;
+		if (m_isMouseEnter == true) {
+			if (Input.mouseScrollDelta.y != 0.0f) {
+				float scaleChangeValue = Input.mouseScrollDelta.y * sm_scaleSpeed;
+				float oldS = Mathf.Pow(m_curScaleArg, 2);
+				m_curScaleArg += scaleChangeValue;
+				if (m_curScaleArg < sm_scaleMin) {
+					m_curScaleArg = sm_scaleMin;
+				}
+				float newS = Mathf.Pow(m_curScaleArg, 2);
+
+				Vector3 scale = m_contentTs.localScale;
+				scale.x = newS;
+				scale.y = newS;
+				m_contentTs.localScale = scale;
+
+				Vector2 oldP = m_contentRTs.anchoredPosition;
+				float posS = newS / oldS;
+				Vector2 newP = new Vector2(oldP.x * posS, oldP.y * posS);
+				m_contentRTs.anchoredPosition = newP;
 			}
-			G.Log("" + scaleChangeValue + " " + m_curScaleArg);
-			Vector3 scale = m_contentTs.localScale;
-			scale.x = Mathf.Pow(m_curScaleArg, 2);
-			scale.y = scale.x;
-			m_contentTs.localScale = scale;
+
+			if (Input.GetMouseButtonDown(MB.MIDDLE) == true) {
+				m_isMiddleDrag = true;
+				m_midDragMousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+				m_midDragNodePos = m_contentTs.position;
+			}
+		}
+
+		if (Input.GetMouseButtonUp(MB.MIDDLE) == true) {
+			m_isMiddleDrag = false;
+		}
+
+		if (m_isMiddleDrag == true) {
+			Vector3 mouseWP = m_camera.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 moveV = mouseWP - m_midDragMousePos;
+			m_contentTs.position = m_midDragNodePos + moveV;
 		}
 	}
 
@@ -57,6 +94,6 @@ public class HContentCtrl : MonoBehaviour {
 	private void OnMouseExit() {
 		m_isMouseEnter = false;
 	}
-	
+
 	#endregion
 }
